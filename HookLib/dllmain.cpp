32d10e7241d77aa27a4330c54ab93047757e9f44
Xun Yang <Programmer-Yang_Xun@outlook.com>
2020-11-01 10:28:44 +0800

    Fix a bug.

--- HookLib/dllmain.cpp
+++ HookLib/dllmain.cpp
@@ -2,22 +2,28 @@
 #include "FileProtector.h"
 
 #pragma data_seg("SharedData")
-HHOOK hHook{};
-struct { WCHAR szHookCallerFileName[UNICODE_STRING_MAX_CHARS]; } sharedData{};
+struct {
+	HHOOK hHook;
+	WCHAR szHookCallerModuleFileName[UNICODE_STRING_MAX_CHARS];
+} sharedData{};
 #pragma data_seg()
 #pragma comment(linker, "/SECTION:SharedData,RWS")
 
 HINSTANCE hInstance;
 
 BOOL WINAPI SetGlobalWindowsHook() {
-	GetModuleFileNameW(NULL, sharedData.szHookCallerFileName, ARRAYSIZE(sharedData.szHookCallerFileName));
-	return hHook == NULL ? (hHook = SetWindowsHookExW(WH_GETMESSAGE, [](int nCode, WPARAM wParam, LPARAM lParam) { return CallNextHookEx(NULL, nCode, wParam, lParam); }, hInstance, 0)) != NULL : FALSE;
+	BOOL ret = sharedData.hHook == NULL;
+	if (ret) {
+		GetModuleFileNameW(NULL, sharedData.szHookCallerModuleFileName, ARRAYSIZE(sharedData.szHookCallerModuleFileName));
+		ret = (sharedData.hHook = SetWindowsHookExW(WH_GETMESSAGE, [](int nCode, WPARAM wParam, LPARAM lParam) { return CallNextHookEx(NULL, nCode, wParam, lParam); }, hInstance, 0)) != NULL;
+	}
+	return ret;
 }
 
 BOOL WINAPI UnhookGlobalWindowsHook() {
-	const BOOL ret = UnhookWindowsHookEx(hHook);
+	const BOOL ret = UnhookWindowsHookEx(sharedData.hHook);
 	if (ret)
-		hHook = NULL;
+		sharedData.hHook = NULL;
 	return ret;
 }
 
@@ -25,7 +31,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpvReserved) {
 	using Hydr10n::FileUtils::FileProtector;
 	switch (dwReason) {
 	case DLL_PROCESS_ATTACH: {
-		FileProtector::Hide(sharedData.szHookCallerFileName);
+		if (sharedData.hHook != NULL)
+			FileProtector::Hide(sharedData.szHookCallerModuleFileName);
 		hInstance = hModule;
 	}	break;
 	}
